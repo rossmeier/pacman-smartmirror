b33c021b73ce3e1e96b456e7308966d8675d43e0
veecue <veecue@ventos.tk>
2019-08-24 18:52:20 +0200

    logging: Output some logs on startup
    No news is good news, but it is always nice to see what is going on.

--- main.go
+++ main.go
@@ -18,17 +18,20 @@ var (
 
 func main() {
 	flag.Parse()
+	log.Printf(`Loading mirrorlist file: "%s"`, *mirrorlistFile)
 	m, err := mirrorlist.FromFile(*mirrorlistFile)
 	if err != nil {
 		log.Fatalf(`Error reading mirrorlist "%s": %v`, *mirrorlistFile, err)
 	}
 
+	log.Printf(`Initing package cache in "%s"`, *cacheDirectory)
 	c, err := cache.New(*cacheDirectory, m)
 	if err != nil {
 		log.Fatalf(`Error initing cache "%s": %v`, *cacheDirectory, err)
 	}
 
 	s := server.New(c)
+	log.Println("Listening on", *listen)
 	err = http.ListenAndServe(*listen, s)
 	if err != nil {
 		log.Fatalf("Error listening on %s: %v", *listen, err)
