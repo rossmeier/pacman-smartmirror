92b6234beb5d9021fec5621b8db3626214608416
Christian Obermaier <ventos@nerdpol.ch>
2019-08-25 13:56:52 +0200

    add functions for parsing repo database files
    Parse pacman repositories .db (.tar.gz) files and extract the relevant
    information for pacman-smartmirror on the fly
    
    Issue: #3

--- database/repository.go
+++ database/repository.go
@@ -1,5 +1,16 @@
 package database
 
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"io"
+	"os"
+
+	"github.com/pkg/errors"
+	"github.com/veecue/pacman-smartmirror/packet"
+)
+
 // Repository describes a Repo as found on an upstream server
 type Repository struct {
 	Name string
@@ -8,4 +19,99 @@ type Repository struct {
 
 func (r Repository) String() string {
 	return r.Arch + "/" + r.Name
+}
+
+// ParseDBFromFile reads a pacman .db file and call cb for each packet directly from File
+func ParseDBFromFile(filename string, cb func(*packet.Packet)) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return errors.Wrap(err, "Error reading file")
+	}
+	defer file.Close()
+
+	return ParseDB(file, cb)
+}
+
+// ParseDBFromFileSlice reads a pacman .db file and creates a []packet.Packet directly from File
+func ParseDBFromFileSlice(filename string) ([]packet.Packet, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, errors.Wrap(err, "Error reading file")
+	}
+	defer file.Close()
+
+	return ParseDBSlice(file)
+}
+
+// ParseDB reads a pacman .db file and will call cb for each packet
+func ParseDB(r io.Reader, cb func(*packet.Packet)) error {
+	zr, err := gzip.NewReader(r)
+	if err != nil {
+		return errors.Wrap(err, "Error gunzipping file")
+	}
+	defer zr.Close()
+
+	return ParseDBGUnzipped(r, cb)
+}
+
+// ParseDBSlice reads a pacman .db file and creates a []packet.Packet
+func ParseDBSlice(r io.Reader) ([]packet.Packet, error) {
+	zr, err := gzip.NewReader(r)
+	if err != nil {
+		return nil, errors.Wrap(err, "Error gunzipping file")
+	}
+	defer zr.Close()
+
+	return ParseDBGUnzippedSlice(zr)
+}
+
+// ParseDBGUnzippedSlice reads a pacman .db file and creates a []packet.Packet
+func ParseDBGUnzippedSlice(r io.Reader) ([]packet.Packet, error) {
+
+	readDb := make([]packet.Packet, 0)
+
+	ParseDBGUnzipped(r, func(p *packet.Packet) {
+		readDb = append(readDb, *p)
+	})
+
+	return readDb, nil
+}
+
+// ParseDBGUnzipped reads a pacman .db file and will call cb for each packet
+func ParseDBGUnzipped(r io.Reader, cb func(*packet.Packet)) error {
+
+	buf := &bytes.Buffer{}
+	reader := tar.NewReader(r)
+	for {
+		pkg, err := reader.Next()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return errors.New("Error while reading tar (DbScratch)")
+		}
+		if pkg.FileInfo().IsDir() {
+			continue
+		}
+		io.Copy(buf, reader)
+		str, err := buf.ReadString('\n')
+		if err != nil {
+			return (err)
+		}
+		if str != "%FILENAME%\n" {
+			return errors.New("Invalid filename designator: " + str)
+		}
+		filename, err := buf.ReadString('\n')
+		if err != nil {
+			return (err)
+		}
+		p, err := packet.FromFilename(filename)
+		if err != nil {
+			return (err)
+		}
+
+		cb(p)
+
+		buf.Reset()
+	}
 }
---
+++ database/repository_test.go
@@ -0,0 +1,190 @@
+package database
+
+import (
+	"archive/tar"
+	"bytes"
+	"log"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const fileGood = `%FILENAME%
+acl-2.2.53-1-x86_64.pkg.tar.xz
+
+%NAME%
+acl
+
+%BASE%
+acl
+
+%VERSION%
+2.2.53-1
+
+%DESC%
+Access control list utilities, libraries and headers
+
+%CSIZE%
+135020
+
+%ISIZE%
+314368
+
+%MD5SUM%
+aaaea535e603f2b55cb320a42cc70397
+
+%SHA256SUM%
+27f4020c77a11992a75b5b99bc1c22797defcea6283b77eb2c311d77b3404443
+
+%PGPSIG%
+iQFCBAABCAAsFiEEAv0cepNOYUVFhJ8ZpiNAdEmOnO4FAlsoqCgOHGFyY2hAZXdvcm0uZGUACgkQpiNAdEmOnO4++ggApA70EbUN/n7Av4UL1fFQLGfMEqL5Fk6mdKbUc1IEHdmcR2C31MWlnUPk2zvbFj8qOpD76G8bq/JerfosPsapOXW+gHdJbI4fAHTYwYqlzymeUPEDkd6/8u/FVwVs0Hdm/0moy6n9c4uKth8XF8d//Ak7i7+klxvNrvoEQM1BsFGLREGbu1ioHf3UmBZLl+kZWtM2Yv0F3F7OPCpLpWCmBhkV3FEVlYDeby3tL3bgaa0NXCbL0uBKcOTAKm1TNNJyTGTh6X8NZUqJTLxApcvF9+7qKeKnN9gEMO9NKjnpkcn+rcweIGITbae1PpTztDhwY60Gtgl7Jjr7F4PwsPN1Pw==
+
+%URL%
+http://savannah.nongnu.org/projects/acl
+
+%LICENSE%
+LGPL
+
+%ARCH%
+x86_64
+
+%BUILDDATE%
+1529391128
+
+%PACKAGER%
+Christian Hesse <arch@eworm.de>
+
+%REPLACES%
+xfsacl
+
+%CONFLICTS%
+xfsacl
+
+%PROVIDES%
+xfsacl
+
+%DEPENDS%
+attr
+`
+
+const gccGood = `%FILENAME%
+gcc-9.1.0-2-x86_64.pkg.tar.xz
+
+%NAME%
+gcc
+
+%BASE%
+gcc
+
+%VERSION%
+9.1.0-2
+
+%DESC%
+The GNU Compiler Collection - C and C++ frontends
+
+%GROUPS%
+base-devel
+
+%CSIZE%
+35571360
+
+%ISIZE%
+145802240
+
+%MD5SUM%
+52953cc05c0b6a112af45006a4f33f62
+
+%SHA256SUM%
+0f4325bd1d88faa05d8b1fa424c213c7270570c6dc61b9c28870c43d379134d6
+
+%PGPSIG%
+iQEzBAABCAAdFiEE82kWh9hnuBtRzgfZu+Q3cUhzKKkFAl0OmrEACgkQu+Q3cUhzKKlP4gf/RMDdvdFMzkxpvjHcHdBVR8WerjwOcfsBRV0JodZEk+0Ecqk1PBobAzgLOxYJPYG1xnvWr9IqCm0/4dwcoEJPbxiKzPGt4POC5DG0M1wQx92dEkCQrA/E6St1IsICbju0zjesS4i0IbraXtWh/CLcXeo4o0VVQrCw0lxycA21ce5NrNWMYHhtH9qSabyoPI3nx9o8v2WZvDxUUkz/V1Rc0u1HKfiTP+UrVuZ9QpCsaQXc6RmzS9oNv5yvGl4BoKZ3yGhmGlflmPcpjAWXQ6AJbKpkScGpjfne8ChtwDGCQm3qcay77ZJo2EjCtoL+Gm2rKEl91lsIyQeW4M+kogWNgw==
+
+%URL%
+https://gcc.gnu.org
+
+%LICENSE%
+GPL
+LGPL
+FDL
+custom
+
+%ARCH%
+x86_64
+
+%BUILDDATE%
+1561233125
+
+%PACKAGER%
+Bartłomiej Piotrowski <bpiotrowski@archlinux.org>
+
+%REPLACES%
+gcc-multilib
+
+%PROVIDES%
+gcc-multilib
+
+%DEPENDS%
+gcc-libs=9.1.0-2
+binutils>=2.28
+libmpc
+
+%OPTDEPENDS%
+lib32-gcc-libs: for generating code for 32-bit ABI
+
+%MAKEDEPENDS%
+binutils
+libmpc
+gcc-ada
+doxygen
+lib32-glibc
+lib32-gcc-libs
+python
+subversion
+
+%CHECKDEPENDS%
+dejagnu
+inetutils`
+
+func createTestTar() []byte {
+	// Create and add some files to the archive.
+	var buf bytes.Buffer
+	tw := tar.NewWriter(&buf)
+	var files = []struct {
+		Name, Body string
+	}{
+		{"acl-2.2.53-1/desc", fileGood},
+		{"gcc-9.1.0-2/desc", gccGood},
+	}
+	for _, file := range files {
+		hdr := &tar.Header{
+			Name: file.Name,
+			Mode: 0600,
+			Size: int64(len(file.Body)),
+		}
+		if err := tw.WriteHeader(hdr); err != nil {
+			log.Fatal(err)
+		}
+		if _, err := tw.Write([]byte(file.Body)); err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err := tw.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	return buf.Bytes()
+}
+
+func TestDBParser(t *testing.T) {
+	database, err := ParseDBGUnzippedSlice(bytes.NewReader(createTestTar()))
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(database))
+	assert.Equal(t, database[0].Name, "acl")
+	assert.Equal(t, database[0].Version, "2.2.53-1")
+	assert.Equal(t, database[0].Arch, "x86_64")
+
+	assert.Equal(t, database[1].Name, "gcc")
+	assert.Equal(t, database[1].Version, "9.1.0-2")
+	assert.Equal(t, database[1].Arch, "x86_64")
+}
