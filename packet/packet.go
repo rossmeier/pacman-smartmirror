c2aa418a1ed55a37e682c5765e68205a9962bba8
veecue <veecue@ventos.tk>
2020-01-01 17:07:04 +0100

    fix: support zst compression
    Recently, arch introduced a new compression (zstd) with the extension
    ".zst". This commit add support for different file extensions.

--- packet/packet.go
+++ packet/packet.go
@@ -7,35 +7,38 @@ import (
 )
 
 var (
-	filenameRegex = regexp.MustCompile(`(.+)-(.+-.+)-(.+)\.pkg\.tar\.xz`)
+	filenameRegex = regexp.MustCompile(`(.+)-(.+-.+)-(.+)\.pkg\.tar\.(xz|zst)`)
 )
 
 // Packet represents a pacman Packet
 type Packet struct {
-	Name    string
-	Version string
-	Arch    string
+	Name        string
+	Version     string
+	Arch        string
+	Compression string
 }
 
 // Filename returns the corresponding filename the packet is saved as
 func (p *Packet) Filename() string {
-	return fmt.Sprintf("%s-%s-%s.pkg.tar.xz",
+	return fmt.Sprintf("%s-%s-%s.pkg.tar.%s",
 		p.Name,
 		p.Version,
 		p.Arch,
+		p.Compression,
 	)
 }
 
 // FromFilename parses a packet's filename and returns the parsed information
 func FromFilename(filename string) (*Packet, error) {
 	matches := filenameRegex.FindStringSubmatch(filename)
-	if len(matches) != 4 {
+	if len(matches) != 5 {
 		return nil, errors.New("Invalid filename")
 	}
 
 	return &Packet{
-		Name:    matches[1],
-		Version: matches[2],
-		Arch:    matches[3],
+		Name:        matches[1],
+		Version:     matches[2],
+		Arch:        matches[3],
+		Compression: matches[4],
 	}, nil
 }
